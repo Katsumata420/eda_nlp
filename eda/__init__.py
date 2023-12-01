d110ba92d3e4c4b7b267985818b633403869717b
Katsumata420 <katsumata-satoru@ed.tmu.ac.jp>
2023-12-01 15:27:33 +0900

    Add version

--- eda/__init__.py
+++ eda/__init__.py
@@ -3,6 +3,6 @@ from .eda_japanese import eda_ja as eda_japanese
 from .tokenizer import get_tokenizer
 from .synonym_extractor import get_synonym_extractor
 
-version = "1.1.0"
+version = "1.1.1"
 
 __all__ = ["eda_english", "eda_japanese", "get_tokenizer", "get_synonym_extractor"]
